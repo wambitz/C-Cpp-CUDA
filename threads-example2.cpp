fb64e6158df338981210503753b6178a4542e2c5
wambee <wambee@hotmail.com>
2018-08-29 13:50:16 -0500

    Added operator overload to call function  in thread t1 when instatiated

--- threads-example2.cpp
+++ threads-example2.cpp
@@ -6,19 +6,45 @@ void PrintMessage()
     std::cout << "Beaty is only skin-depp" << std::endl;
 }
 
+class Functor 
+{
+public:
+    // void operator()(std::string message)
+    void operator()()
+    {
+        for (int i = 0; i > -100; i--)
+        {
+            std::cout << "from t1: " << i << std::endl;
+        }
+        // std::cout << "t1 says: " << msg << std::endl;
+    }
+
+};
+
 int main(int argc, char const *argv[])
 {
-    std::thread t1(PrintMessage);   // t1 starts running
-    // t1.join();                   // main thread waits for t1 to finish
-    t1.detach();                    // t1 will freely rumn on its own -- daemon process 
+    Functor fct;
+    // std::string s = "Where is no trust, there is no love";
+    // std::thread t1((Functor()),s);   // t1 starts running
+    std::thread t1(fct);   // t1 starts running
+    // using RAII
+    // Wrapper w(t1);
 
-    //... later on
-
-    // t1.join();                       // crash
-    if (t1.joinable())
+    try
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            // std::cout << "from main: " << s << std::endl;
+            std::cout << "from main: " << i << std::endl;
+        }
+    }
+    catch (...)
     {
         t1.join();
+        throw;
     }
+
+    t1.join();
 
     return 0;
 }
