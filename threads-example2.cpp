63509bc6d5b79b4ec96881d6cfd9ab2a4714cda4
wambee <wambee@hotmail.com>
2018-08-30 13:38:19 -0500

    - Functor class updated - Instance syntax updated - Modified for loops for string outputs

--- threads-example2.cpp
+++ threads-example2.cpp
@@ -9,34 +9,21 @@ void PrintMessage()
 class Functor 
 {
 public:
-    // void operator()(std::string message)
-    void operator()()
+    void operator()(std::string msg)
     {
-        for (int i = 0; i > -100; i--)
-        {
-            std::cout << "from t1: " << i << std::endl;
-        }
-        // std::cout << "t1 says: " << msg << std::endl;
+        std::cout << "t1 says: " << msg << std::endl;
     }
 
 };
 
 int main(int argc, char const *argv[])
 {
-    Functor fct;
-    // std::string s = "Where is no trust, there is no love";
-    // std::thread t1((Functor()),s);   // t1 starts running
-    std::thread t1(fct);   // t1 starts running
-    // using RAII
-    // Wrapper w(t1);
-
+    std::string s = "Where is no trust, there is no love";
+    std::thread t1((Functor()), s);   // t1 starts running
+    
     try
     {
-        for (int i = 0; i < 100; i++)
-        {
-            // std::cout << "from main: " << s << std::endl;
-            std::cout << "from main: " << i << std::endl;
-        }
+        std::cout << "from main: " << s << std::endl;
     }
     catch (...)
     {
