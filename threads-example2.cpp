f6a6c62259ac3c1a0842fab86382879d62b70d8d
wambee <wambee@hotmail.com>
2018-08-30 18:22:02 -0500

    Changed ownership of thread

--- threads-example2.cpp
+++ threads-example2.cpp
@@ -20,9 +20,15 @@ public:
 int main(int argc, char const *argv[])
 {
     std::string s = "Where is no trust, there is no love";
-    std::thread t1((Functor()), std::ref(s));   // t1 starts running
+    std::cout << std::this_thread::get_id() << std::endl;
+
+    std::thread t1((Functor()), std::move(s));   // t1 starts running
+    // std::thread t2 = t1; // this won't compile, threads can not be copied
+    std::cout << t1.get_id() << std::endl;
     
-    t1.join();
+    std::thread t2 = std::move(t1); // this is ok but t1 will be empty
+    
+    t2.join();
     std::cout << "from main: " << s << std::endl;
 
     return 0;
