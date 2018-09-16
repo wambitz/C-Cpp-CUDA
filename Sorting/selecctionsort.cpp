c2eca1658e9b52c6e15f92345d3daeb2c1a74b01
wambee <wambee@hotmail.com>
2018-09-15 20:24:08 -0500

    fix: fixed logic errors

--- Sorting/selecctionsort.cpp
+++ Sorting/selecctionsort.cpp
@@ -8,25 +8,29 @@ void selection_sort(int* p_array, int size)
     int j = 0;
 
     /* minimum value */
-    int min = 0;
+    int minimum_value_index = 0;
+    /* swaping variable*/
+    int swap = 0;
     
     /* 1. for i <-0 to n-2 do */
     for (i = 0; i < size-1; i++)
     {
         /* 2. min <- i*/
-        min = i;
+        minimum_value_index = i;
     
         /*3. for j <- i+1 to n-1 */
         for (j = i+1; j < size; j++)
         {
             /* 4. if A[j] < A[min], min <- j */
-            if (p_array[j] < p_array[min])
+            if (p_array[j] < p_array[minimum_value_index])
             {
-                /* 5. swap A[i] and A[min] */
-                p_array[i] = p_array[min];
+                minimum_value_index = j;
             }
         }
-
+        /* 5. swap A[i] and A[min] */
+        swap = p_array[i];
+        p_array[i] = p_array[minimum_value_index];
+        p_array[minimum_value_index] = swap;
     }
 }
 
