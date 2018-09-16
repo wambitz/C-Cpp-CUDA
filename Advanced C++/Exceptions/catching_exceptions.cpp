017b1e15a2fc7965980279486312a49a9cec36c1
wambee <wambee@hotmail.com>
2018-09-16 17:35:59 -0500

    chore: Renamed Exceptions folder and moved examples

---
+++ Advanced C++/Exceptions/catching_exceptions.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+void GoesWrong()
+{
+    bool error1Detected = true;
+    bool error2Detected = false;
+
+    if (error1Detected)
+    {
+        throw bad_alloc();
+    }
+
+    if (error2Detected)
+    {
+        throw exception();
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    try
+    {
+        GoesWrong();
+    }
+    // if a parent class is called before the exception
+    // might not work as expected because of polymorfism
+    // always call derived classes before.
+    catch(bad_alloc &error)
+    {
+        cout << "Catching bad_alloc: " << error.what() << endl;
+    }
+    catch(exception &error)
+    {
+        cout << "Catching exception: " << error.what() << endl;
+    }
+    
+    return 0;
+}
